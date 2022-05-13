51940e8f4c06c1aae7087e341ba2a4c3b354ea2d
unknown <bajiraouttamsinha@gmail.com>
2022-05-13 13:30:17 +0530

    fetching details after search button click

--- static/recommend.js
+++ static/recommend.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // Button will be disabled until we type anything inside the input field
+
   const inputField = document.getElementById('autoComplete');
   const inputHandler = function(e) {
     if(e.target.value == ""){
@@ -25,31 +25,30 @@ $(document).ready(function() {
 });
 
 
-// based on the name of the movie fetch the details of movie
 function load_details(my_api_key,title){
   console.log(title);
-  // $.ajax({
-  //   type: 'GET',
-  //   url:'https://api.themoviedb.org/3/search/movie?api_key='+my_api_key+'&query='+title,
+  $.ajax({
+    type: 'GET',
+    url:'https://api.themoviedb.org/3/search/movie?api_key='+my_api_key+'&query='+title,
 
-  //   success: function(movie){
-  //     if(movie.results.length<1){
-  //       $('.fail').css('display','block');
-  //       $('.results').css('display','none');
-  //       $("#loader").delay(500).fadeOut();
-  //     }
-  //     else{
-  //       $("#loader").fadeIn();
-  //       $('.fail').css('display','none');
-  //       $('.results').delay(1000).css('display','block');
-  //       var movie_id = movie.results[0].id;
-  //       var movie_title = movie.results[0].original_title;
-  //       movie_recs(movie_title,movie_id,my_api_key);
-  //     }
-  //   },
-  //   error: function(e){
-  //     console.log('Unalbe to load Details');
-  //     $("#loader").delay(500).fadeOut();
-  //   },
-  // });
+    success: function(movie){
+      if(movie.results.length<1){
+        $('.failed').css('display','block');
+        $('.success').css('display','none');
+        $("#loader").delay(500).fadeOut();
+      }
+      else{
+        $("#loader").fadeIn();
+        $('.failed').css('display','none');
+        $('.success').delay(1000).css('display','block');
+        var movie_id = movie.results[0].id;
+        var movie_title = movie.results[0].original_title;
+        movie_recs(movie_title,movie_id,my_api_key);
+      }
+    },
+    error: function(e){
+      console.log('Unalbe to load Details');
+      $("#loader").delay(500).fadeOut();
+    },
+  });
 }
