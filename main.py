4fa06659c24f982c2623b8e852045ea2b09fd4de
unknown <bajiraouttamsinha@gmail.com>
2022-05-14 00:37:50 +0530

    adding cosing simlarity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
-import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
@@ -9,11 +10,52 @@ def get_movies():
     return list(orig_data['movie_title'].str.capitalize())
 
 
+def create_similarity():
+    orig_data = pd.read_csv('./datasets/processedData_2017.csv')
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(orig_data['processedCol'])
+    similarity = cosine_similarity(count_matrix)
+    return orig_data,similarity
+
+
+def getRecommendedMoviesTitles(mov):
+    mov = mov.lower()  #because all the data present in our dataset is in lower case
+    try:
+        data.head()
+        similarity.shape
+    except:
+        data, similarity = create_similarity()
+    # if the movie title that we are quering in createsimilarity didn't matched with any movie in our dataset
+    if mov not in data['movie_title'].unique():
+        return('Sorry! Your request can not be completed at the movement. Please check if you spelt everything correctly or try with some other movies')
+    else:
+        i = data.loc[data['movie_title']==mov].index[0]
+        lst = list(enumerate(similarity[i])) #indexing each similarity 
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+        lst = lst[1:11] # excluding the first item because it is the requested movie itself
+        finalList = []
+        for i in range(len(lst)):
+            item = finalList[i][0]
+            finalList.append(data['movie_title'][item])
+        return finalList
+    
+
+@app.route("/createsimilarity",methods=["POST"])
+def get_similarity():
+    movie = request.form['title']
+    recommended_movie_titles = getRecommendedMoviesTitles(movie)
+    print(recommended_movie_titles)
+    if type(recommended_movie_titles)==type('string'):
+        return recommended_movie_titles
+    else:
+        m_str=" || ".join(recommended_movie_titles)
+        return m_str
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     movies = get_movies()
-    print(movies[:10])
     return render_template('home.html',movies=movies)
 
 
--- static/recommend.js
+++ static/recommend.js
@@ -12,43 +12,74 @@ $(document).ready(function() {
   inputField.addEventListener('input', inputHandler);
 
   $('.search-button').on('click',function(){
-    var my_api_key = '97933c59065a2f21b4f313c8ef927b47';
+    var API_KEY = 'API_KEY';
     var entered_title = $('.input').val();
     if (entered_title=="") {
       $('.success').css('display','none');
       $('.failed').css('display','block');
     }
     else{
-      load_details(my_api_key,entered_title);
+      searchMovie(API_KEY,entered_title);
     }
   });
 });
 
 
-function load_details(my_api_key,title){
-  console.log(title);
+function searchMovie(API_KEY,title){
   $.ajax({
     type: 'GET',
-    url:'https://api.themoviedb.org/3/search/movie?api_key='+my_api_key+'&query='+title,
+    url:'https://api.themoviedb.org/3/search/movie?api_key='+API_KEY+'&query='+title,
 
     success: function(movie){
+      //  if we didn't get any movie details to show
       if(movie.results.length<1){
         $('.failed').css('display','block');
         $('.success').css('display','none');
         $("#loader").delay(500).fadeOut();
       }
+      // if we got some movies to show
       else{
         $("#loader").fadeIn();
         $('.failed').css('display','none');
         $('.success').delay(1000).css('display','block');
-        var movie_id = movie.results[0].id;
-        var movie_title = movie.results[0].original_title;
-        movie_recs(movie_title,movie_id,my_api_key);
+        var id = movie.results[0].id; // fetching the first element of thr response array, to be more accurate
+        var title = movie.results[0].original_title;
+        getSimilarMovies(title,id,API_KEY);
       }
     },
-    error: function(e){
-      console.log('Unalbe to load Details');
+    error: function(){
+      console.log('Something went wrong in searchMovie func');
       $("#loader").delay(500).fadeOut();
     },
   });
+}
+
+// making a POST req by passing the title of the movie in the body to get similar movies
+function getSimilarMovies(title,id,API_KEY){
+  $.ajax({
+    type:'POST',
+    url:"/createsimilarity",
+    data:{'title':title},
+    success: function(recs){
+      // if there is no similar data
+      if(recs=="Sorry! Your request can not be completed at the movement. Please check if you spelt everything correctly or try with some other movies"){
+        $('.failed').css('display','block');
+        $('.success').css('display','none');
+        $("#loader").delay(500).fadeOut();
+      }
+      else {
+        $('.failed').css('display','none');
+        $('.success').css('display','block');
+        var movie_arr = recs.split(' || ');
+        var results = [];
+        for(const movie in movie_arr){
+          results.push(movie_arr[movie]);
+        }
+      }
+    },
+    error: function(){
+      console.log("Something went wrong in getSimilarMovies func");
+      $("#loader").delay(500).fadeOut();
+    },
+  }); 
 }
