1f6938b5c82b19a5d3846ba389178565a6358d55
unknown <bajiraouttamsinha@gmail.com>
2022-05-28 18:36:26 +0530

    deploy

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 ####### Connecting Database (MongoDB) #####
 ##############################################
 try:
-    mongo = pymongo.MongoClient('__MONGO_URL__')
+    mongo = pymongo.MongoClient('mongodb+srv://Uttam:QJLRGuIVeozVj7yQ@cluster0.axnkt.mongodb.net/?retryWrites=true&w=majority')
     db = mongo.user
     mongo.server_info()
 except Exception as e:
